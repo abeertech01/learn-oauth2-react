2e95042cba46ae815a6077ed410946cc482ead12
Abeer <abeer.technology@gmail.com>
2022-07-28 13:47:18 +0600

    5th commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Homepage from "./Components/Homepage/Homepage"
 import Loginpage from "./Components/Loginpage/Loginpage"
 import NavBar from "./Components/NavBar/NavBar"
 
+import "./GlobalStyles.css"
+
 function App() {
   return (
     <BrowserRouter>
