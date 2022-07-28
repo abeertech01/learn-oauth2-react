bcc3ff5dffc73258f2dff025eae8312408c331db
Abeer <abeer.technology@gmail.com>
2022-07-28 10:59:29 +0600

    2nd commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,32 @@
 import express from "express"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 
 mongoose.connect(
-  "mongodb+srv://memories123:memories123@cluster0.6xni6.mongodb.net/oauth2?retryWrites=true&w=majority",
+  `${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`,
   {},
   () => {
     console.log("Connected to mongoose successfully")
   }
 )
+
+// Middlewares
+app.use(express.json())
+app.use(cors({ origin: "http://localhost:2000", credentials: true }))
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+)
+app.use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("/", (req, res) => {
   res.send("Hello World")
